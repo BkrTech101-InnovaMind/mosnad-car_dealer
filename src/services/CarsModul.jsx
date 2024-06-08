da7db721eb99a244589c555d8f55701ec29d396e
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-08 05:39:06 +0300

    implement cars filter that will be used in the searchbar

--- src/services/CarsModul.jsx
+++ src/services/CarsModul.jsx
@@ -1,14 +1,15 @@
+// /services/CarsModul.jsx
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export const CarsModul = () => {
-  const [cars, setCars] = useState([]);
+export const CarsModul = ({ filter }) => {
+  const [carsList, setCarsList] = useState([]);
   useEffect(() => {
     axios.get("cars.json").then((res) => {
-      console.log(res.data);
-      setCars(res.data);
+      setCarsList(res.data);
     });
   }, []);
 
+  const cars = filter ? carsList.filter(filter) : carsList;
   return cars;
 };
