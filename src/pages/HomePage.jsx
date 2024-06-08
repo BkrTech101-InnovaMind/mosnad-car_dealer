25722ccb0c369a893ec9211bd2cdca31aaf8fbce
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-08 05:39:59 +0300

    implement the cars filter function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CarCard from "../components/CarCard";
 import { CarsModul } from "../services/CarsModul";
+import Navbar from "../components/Navbar";
 
 export default function HomePage() {
-  const cars = CarsModul();
+  const [query, setQuery] = useState("");
+
+  const filterCars = (car) => {
+    if (!query) return true;
+    const lowerCaseQuery = query.toLowerCase();
+    return (
+      car.company_brand.toLowerCase().includes(lowerCaseQuery) ||
+      car.sub_details.model.toLowerCase().includes(lowerCaseQuery) ||
+      car.sub_details.year.toString().includes(lowerCaseQuery) ||
+      car.price.toString().replace(/,/g, "").includes(lowerCaseQuery)
+    );
+  };
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-      {cars.map((car) => (
-        <CarCard key={car.id} car={car} />
-      ))}
-    </div>
+    <>
+      <Navbar onSearch={setQuery} />
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {CarsModul({ filter: filterCars }).map((car) => (
+          <CarCard key={car.id} car={car} />
+        ))}
+      </div>
+    </>
   );
 }
