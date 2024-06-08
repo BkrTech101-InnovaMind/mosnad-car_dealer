064d3231640f61156558d75818a048f475c1ec4f
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-08 06:40:56 +0300

    initialize the details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
       </Routes>
     </Router>
   );
