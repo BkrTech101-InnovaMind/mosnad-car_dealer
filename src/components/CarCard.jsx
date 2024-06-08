d65b25f56b47e7e5952c6ed46df77c92c2b039ad
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-08 06:41:50 +0300

    change the link href to be to and pass the car details to the details page

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -10,21 +10,20 @@ export default function CarCard({ car }) {
         alt={`${car.company_brand} ${car.sub_details.model} ${car.sub_details.year}`}
       />
       <div className="px-5 pb-5">
-        <Link href="#">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            {car.company_brand} {car.sub_details.model} {car.sub_details.year}
-          </h5>
-        </Link>
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          {car.company_brand} {car.sub_details.model} {car.sub_details.year}
+        </h5>
         <div className="my-2">{car.sub_details.seller_details.address}</div>
         <div className="flex items-center justify-between">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${car.price}
           </span>
           <Link
-            href="#"
+            to={`details/${car.id}`}
+            state={{ car }}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add to cart
+            Show Details
           </Link>
         </div>
       </div>
