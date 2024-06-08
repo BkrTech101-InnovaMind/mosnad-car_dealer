667b561f29a9cdbb530916b3561037134a9dedaf
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-08 05:41:34 +0300

    remove the layout call so that it's not needed as we have only 2 pages in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./layout/Layout";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
