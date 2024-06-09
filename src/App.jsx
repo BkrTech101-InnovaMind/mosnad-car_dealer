16f7dcf1235d3ab33ab5f5acc1b2d5c2a2c7f509
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-09 17:06:51 +0300

    initialize add car page rout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
+import AddCarPage from "./pages/AddCarPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/details/:id" element={<DetailsPage />} />
+        <Route path="/add_car" element={<AddCarPage />} />
       </Routes>
     </Router>
   );
