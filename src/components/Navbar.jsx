96d57dd31a54c084fe98887332723a87d14253d6
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-09 17:07:06 +0300

    implement add car link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar({ onSearch }) {
           <ul className="flex p-0 mt-4 font-medium space-x-8 rtl:space-x-reverse md:mt-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="#"
+                to="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -32,7 +32,7 @@ export default function Navbar({ onSearch }) {
             </li>
             <li>
               <Link
-                href="#"
+                to="/add_car"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Add Car
