0aedbbb54debab4e32a10ea6a1d3765c838c56e9
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-06-09 11:48:56 +0300

    implement a custom hook to fetch the cars needed keys to be filters

--- src/services/CarsModul.jsx
+++ src/services/CarsModul.jsx
@@ -13,3 +13,27 @@ export const CarsModul = ({ filter }) => {
   const cars = filter ? carsList.filter(filter) : carsList;
   return cars;
 };
+
+export const useCarData = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        const cars = res.data;
+        const transformedData = [
+          { Company: [...new Set(cars.map((car) => car.company_brand))] },
+          { Model: [...new Set(cars.map((car) => car.sub_details.model))] },
+          { Year: [...new Set(cars.map((car) => car.sub_details.year))] },
+        ];
+        setData(transformedData);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, [url]);
+
+  return { data, error };
+};
